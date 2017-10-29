ccfe2b37cef616784453fea64a9de0a61f2899de
Tara Grimm <tararochegrimm@gmail.com>
2017-10-29 13:40:31 -0400

    Added a Navbar

---
+++ src/common/main.component.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "React";
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#">Scotch Cars</a>
+                        </div>
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+                                <li><a href="#">Link</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className="container">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main
--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Router, Route } from 'react-router';
 
+import Main from "./common/main.component.jsx";
 import Home from "./common/home.component.jsx";
 import About from "./common/about.component.jsx";
 import Car from "./car/car.component.jsx";
 
 render(
     <Router>
-        <Route path="/" component={Home} />
-        <Route path="/cars" component={Car}/>
-        <Route path="/about" component={About}/>
+        <Route component={Main}>
+            <Route path="/" component={Home} />
+            <Route path="/cars" component={Car} />
+            <Route path="/about" component={About} />
+        </Route>
     </Router>,
     document.getElementById('container')
 );
